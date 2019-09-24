beb30a71f86f2bc751b32d1ba9e3b292830b1a99
Michael Hewitt <michael@hewitts.us>
2019-09-23 20:08:00 -0700

    ENS-45: Eliminate level of indirection for church in this_week

--- this_week.py
+++ this_week.py
@@ -2,11 +2,10 @@
 '''Publish one or more songs to this week folder
 
 Usage:
-  this_week.py <church> <song-prefix>...
-  this_week.py <church> --clean
+  this_week.py <song-prefix>...
+  this_week.py --clean
 
 Arguments:
-  <church>           name of church
   <song-prefix>      prefix of song name to publish
 
 Options:
@@ -26,22 +25,27 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 def copy_song_to_this_week(church, prefix):
 	hymnals_dir = os.path.join(script_dir, '..', 'hymnals')
-	this_week_dir = os.path.join(script_dir, church)
+	# this_week_dir = os.path.join(script_dir, church)
+	this_week_dir = script_dir
 	if not os.path.exists(this_week_dir):
 		os.makedirs(this_week_dir)
 	found = False
-	for hymnal_dir in os.listdir(hymnals_dir):
-		hymnal_dir_path = os.path.join(hymnals_dir, hymnal_dir)
-		if os.path.isdir(hymnal_dir_path):
-			for filename in os.listdir(hymnal_dir_path):
-				filename_path = os.path.join(hymnal_dir_path, filename)
-				if filename.startswith(prefix) and os.path.isfile(filename_path):
-					found = True
-					print('Copying ' + filename)
-					this_week_song = os.path.join(this_week_dir, filename)
-					import_path = os.path.join('..', '..', 'hymnals', hymnal_dir, filename)
-					with open(this_week_song, "w") as file:
-						file.write('import ' + import_path + os.linesep)
+	# for hymnal_dir in os.listdir(hymnals_dir):
+	# 	hymnal_dir_path = os.path.join(hymnals_dir, hymnal_dir)
+	# 	if os.path.isdir(hymnal_dir_path):
+	# 		for filename in os.listdir(hymnal_dir_path):
+	hymnal_dir = hymnals_dir
+	hymnal_dir_path = hymnal_dir
+	for filename in os.listdir(hymnal_dir_path):
+		filename_path = os.path.join(hymnal_dir_path, filename)
+		if filename.startswith(prefix) and os.path.isfile(filename_path):
+			found = True
+			print('Copying ' + filename)
+			this_week_song = os.path.join(this_week_dir, filename)
+			# import_path = os.path.join('..', 'hymnals', hymnal_dir, filename)
+			import_path = os.path.join('..', 'hymnals', filename)
+			with open(this_week_song, "w") as file:
+				file.write('import ' + import_path + os.linesep)
 
 
 	if not found:
@@ -49,13 +53,16 @@ def copy_song_to_this_week(church, prefix):
 
 def main():
 	arguments = docopt.docopt(__doc__)
-	church = arguments['<church>']
-	if not os.path.isdir(church):
-		raise RuntimeError('Church not found: ' + church)
+	# church = arguments['<church>']
+	# if not os.path.isdir(church):
+	# 	raise RuntimeError('Church not found: ' + church)
+	church = ''
 	if arguments['--clean']:
-		church_songs = os.path.join(script_dir, church, '*')
+		# church_songs = os.path.join(script_dir, church, '*')
+		church_songs = os.path.join(script_dir, '*')
 		for file in glob.glob(church_songs):
-			os.remove(file)
+			if not file.endswith('py'):
+				os.remove(file)
 	else:
 		for prefix in arguments['<song-prefix>']:
 			copy_song_to_this_week(church, prefix)
